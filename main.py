442d9c8f7ede4a116127ca69bc7f6d255e5b7d7a
siddhantkumbhar107 <siddhantkumbhar107@gmail.com>
2026-03-18 17:37:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+keywords = ["/JS", "/JavaScript", "/AA", "/OpenAction", "/AcroForm", "/RichMedia", "/Launch", "/EmbeddedFile"]
+from flask import Flask, request, jsonify
+import os
+import sys
+from analyzer import analyze_pdf,get_object_count,calculate_risk,extract_iocs
+from metadata import extract_metadata
+from ioc_extractor import extract_iocs
+from report_generator import generate_report
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "PDF Malware Analysis API is running!"
+def main():
+
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py file.pdf")
+        return
+
+    file = sys.argv[1]
+    print("\n[+] Starting PDF Malware Analysis\n")
+    with open(file, 'rb') as f:
+        raw_content = f.read()
+    metadata = extract_metadata(file)
+    iocs = extract_iocs(file)
+    analysis = analyze_pdf(file)
+    obj_count = get_object_count(file)
+    risk_level, risk_reasons = calculate_risk(metadata, analysis, obj_count)
+    ips = extract_iocs(raw_content)
+    generate_report(file, metadata, analysis, iocs, obj_count, risk_level, risk_reasons)
+
+def analyze_pdf(file):
+    # Read the file content directly from memory
+    content = file.read().decode(errors="ignore")
+    file.seek(0)  # Move back to the start of the file for the next function
+    
+    suspicious = []
+    for key in keywords:
+        if key in content:
+            suspicious.append(key)
+    return suspicious
+
+def get_object_count(file):
+    try:
+        # Read the raw bytes directly
+        content = file.read()
+        file.seek(0)  # Move back to the start again
+        count = content.count(b' obj')
+        return count
+    except Exception as e:
+        return f"Error: {e}"
+if __name__ == "__main__":
+     main()
