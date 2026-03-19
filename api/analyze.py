2a197e596ae086dd59b0c7e6f08e40fcfe01d095
siddhantkumbhar107 <siddhantkumbhar107@gmail.com>
2026-03-19 19:09:35 +0530

    Create analyze.py

---
+++ api/analyze.py
@@ -0,0 +1,22 @@
+import json
+from analyzer import analyze_pdf
+
+def handler(request):
+    if request.method == "POST":
+        file = request.files["file"]
+
+        filepath = "/tmp/temp.pdf"
+        file.save(filepath)
+
+        result = analyze_pdf(filepath)
+
+        return {
+            "statusCode": 200,
+            "headers": {"Content-Type": "application/json"},
+            "body": json.dumps(result)
+        }
+
+    return {
+        "statusCode": 405,
+        "body": "Method Not Allowed"
+    }
