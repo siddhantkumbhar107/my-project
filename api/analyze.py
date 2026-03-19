696f9c5fc02c53b3baafbaf2eb75a3578cd26916
siddhantkumbhar107 <siddhantkumbhar107@gmail.com>
2026-03-19 19:23:09 +0530

    Update analyze.py

--- api/analyze.py
+++ api/analyze.py
@@ -1,6 +1,6 @@
 import json
 from analyzer import analyze_pdf
-
+from http.server import BaseHTTPRequestHandler
 def handler(request):
     if request.method == "POST":
         file = request.files["file"]
