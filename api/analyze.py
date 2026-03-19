bbc583605001c93f8253982f6ae34b73887b2502
siddhantkumbhar107 <siddhantkumbhar107@gmail.com>
2026-03-19 19:27:25 +0530

    Update analyze.py

--- api/analyze.py
+++ api/analyze.py
@@ -2,21 +2,7 @@ import json
 from analyzer import analyze_pdf
 from http.server import BaseHTTPRequestHandler
 def handler(request):
-    if request.method == "POST":
-        file = request.files["file"]
-
-        filepath = "/tmp/temp.pdf"
-        file.save(filepath)
-
-        result = analyze_pdf(filepath)
-
-        return {
-            "statusCode": 200,
-            "headers": {"Content-Type": "application/json"},
-            "body": json.dumps(result)
-        }
-
     return {
-        "statusCode": 405,
-        "body": "Method Not Allowed"
+        "statusCode": 200,
+        "body": "API is working"
     }
