b2dc51cd787ee209d32c8825de30c1399aff92bf
siddhantkumbhar107 <siddhantkumbhar107@gmail.com>
2026-03-20 13:00:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import sys
 import os
 from flask import Flask, request, render_template, jsonify
 from analyzer import analyze_pdf
+sys.path.append(os.path.dirname(__file__))
 from flask import Flask
 app = Flask(__name__)
-
 @app.route('/')
 def home():
     return "index.html"
