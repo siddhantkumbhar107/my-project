4a0dd05c8b3e13c428138fbd10ae851a236d90c5
siddhantkumbhar107 <siddhantkumbhar107@gmail.com>
2026-03-20 14:21:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-import sys
 import os
 from flask import Flask, request, render_template, jsonify
 from analyzer import analyze_pdf
-sys.path.append(os.path.dirname(__file__))
+
 app = Flask(__name__)
+
 @app.route('/')
 def home():
-    return render_template"index.html"
-
+    return render_template("index.html")
 
 @app.route('/analyze', methods=['POST'])
 def analyze():
-    file = request.files['file']
+    file = request.files.get('file')
+
+    if not file:
+        return "No file uploaded"
 
     filepath = "temp.pdf"
     file.save(filepath)
@@ -20,10 +22,12 @@ def analyze():
 
     return render_template("result.html", result=result)
 
-
 @app.route('/api/analyze', methods=['POST'])
 def api_analyze():
-    file = request.files['file']
+    file = request.files.get('file')
+
+    if not file:
+        return jsonify({"error": "No file uploaded"})
 
     filepath = "temp.pdf"
     file.save(filepath)
@@ -31,7 +35,6 @@ def api_analyze():
     result = analyze_pdf(filepath)
 
     return jsonify(result)
-
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
